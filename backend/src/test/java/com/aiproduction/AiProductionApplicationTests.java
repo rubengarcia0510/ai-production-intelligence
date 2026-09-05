49dcffe7405f25b3d7cfb965165fda8897ba6a92
Ruben Garcia <rubengarcia0510@gmail.com>
2026-09-05 08:23:07 -0300

    chore: fix foundation test and frontend dependencies

--- backend/src/test/java/com/aiproduction/AiProductionApplicationTests.java
+++ backend/src/test/java/com/aiproduction/AiProductionApplicationTests.java
@@ -3,11 +3,12 @@ package com.aiproduction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+)
 class AiProductionApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
